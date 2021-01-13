1ae32bc5d82ea6e64fd290dcbe98fc1931d8854f
RudyPark3091 <grayblack313@gmail.com>
2021-01-14 01:28:41 +0900

    Update beep.sh

--- shell/beep.sh
+++ shell/beep.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # for WSL system
-for i in seq 1 5; do
-	powershell.exe '[console]::beep(1000,500)'
+for i in $(seq 1 5); do
+	powershell.exe '[console]::beep(1000,1000)'
+	sleep 0.5
 done
