f107861bad56cfb2cbcca2475ca9d7c7f7ef8d7f
RudyPark3091 <grayblack313@gmail.com>
2021-01-13 21:39:03 +0900

    Update mst/

--- algorithm/mst/main.cpp
+++ algorithm/mst/main.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 #include <vector>
 #include <tuple>
-#include <algorithm>
 #include "mst.cpp"
 using namespace std;
 
@@ -15,31 +13,8 @@ vector<tuple<int, int, int>> edges = {
 	{5, 6, 25},
 };
 
-bool cmp(tuple<int, int, int> a, tuple<int, int, int> b) {
-	return get<2>(a) < get<2>(b);
-}
-
 int main() {
-	int cost_sum = 0;
-
-	for (int i = 0; i < 7; i++) {
-		parent[i] = i;
-	}
-	sort(edges.begin(), edges.end(), cmp);
-
-	for (auto edge : edges) {
-		int a = get<0>(edge);
-		int b = get<1>(edge);
-		int cost = get<2>(edge);
-
-		if (find_parent(parent, a) != find_parent(parent, b)) {
-			_union(parent, a, b);
-			cout << "add edge between vertex " << a+1
-				<< " and " << b+1 << endl;
-			cost_sum += cost;
-		}
-	}
-	cout << "cost sum for building MST: " << cost_sum << endl;
+	kruskal(parent, edges);
 	return 0;
 }
 
--- algorithm/mst/mst.cpp
+++ algorithm/mst/mst.cpp
@@ -1,6 +1,9 @@
 // mst: minimum-spanning-tree
 
+#include <iostream>
 #include <vector>
+#include <algorithm>
+#include <tuple>
 using namespace std;
 
 int find_parent(vector<int>& _parent, int x) {
@@ -15,3 +18,28 @@ void _union(vector<int>& _parent, int a, int b) {
 	else _parent[b] = a;
 };
 
+bool cmp(tuple<int, int, int> a, tuple<int, int, int> b) {
+	return get<2>(a) < get<2>(b);
+}
+
+void kruskal(vector<int>& _parent, vector<tuple<int, int, int>>& _edges) {
+	int cost_sum = 0;
+	for (int i = 0; i < 7; i++) {
+		_parent[i] = i;
+	}
+	sort(_edges.begin(), _edges.end(), cmp);
+
+	for (auto edge : _edges) {
+		int a = get<0>(edge);
+		int b = get<1>(edge);
+		int cost = get<2>(edge);
+
+		if (find_parent(_parent, a) != find_parent(_parent, b)) {
+			_union(_parent, a, b);
+			cout << "add edge between vertex " << a+1
+				<< " and " << b+1 << endl;
+			cost_sum += cost;
+		}
+	}
+	cout << "cost sum for building MST: " << cost_sum << endl;
+}
