c1ad0456ee11d50f3c9deeb4f06451990368c765
RudyPark3091 <grayblack313@gmail.com>
2021-01-21 02:17:36 +0900

    Update shell/clock.sh

--- shell/clock.sh
+++ shell/clock.sh
@@ -2,10 +2,16 @@
 
 source ./number.sh
 
-: << desc
+fin=0
 
-desc
+exitfn() {
+	echo -e "\\e[?25h"
+	tput rmcup
+	fin=1
+}
+
 tput smcup
+trap exitfn SIGINT
 
 x=$(tput cols)
 y=$(tput lines)
@@ -19,14 +25,34 @@ d=$(date)
 ptime=${d:11:5}
 phour=${time:0:2}
 pminute=${time:3:2}
-dt=0
 
 echo -e $(divider $(( midx + 16 )) $midy)
-while true; do
+while [ $fin -eq 0 ]; do
 	d=$(date)
 	time=${d:11:5}
 	hour=${time:0:2}
 	minute=${time:3:2}
+
+	nx=$(tput cols)
+	ny=$(tput lines)
+
+	if ! [ $nx -eq $x ]; then
+		echo -e "\e[2J"
+		x=$nx
+		(( midx = x / 2 - 17 ))
+		phour=0
+		pminute=0
+		echo -e $(divider $(( midx + 16 )) $midy)
+	fi
+
+	if ! [ $ny -eq $y ]; then
+		echo -e "\e[2J"
+		y=$ny
+		(( midy = y / 2 - 2 ))
+		phour=0
+		pminute=0
+		echo -e $(divider $(( midx + 16 )) $midy)
+	fi
 
 	echo -e "\\e[?25l"
 	if ! [[ "$phour" = "$hour" ]]; then
@@ -43,12 +69,6 @@ while true; do
 	echo -e "$(move $(( midy + 6 )) $(( midx + 5 )))$d"
 
 	sleep 1
-	(( dt = dt + 1 ))
-	if ! [ -z $1 ]; then
-		if [ $dt -gt $1 ]; then break; fi
-	else
-		if [ $dt -gt 0 ]; then break; fi
-	fi
 done
 
 echo -e "\\e[?25h"
