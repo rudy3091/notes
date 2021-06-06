1d6a7494702b2793559cd5864869f865e8186f2e
rudy3091 <grayblack313@gmail.com>
2021-06-06 22:24:36 +0900

    Add reference link

--- rust/src/map.rs
+++ rust/src/map.rs
@@ -19,7 +19,8 @@ fn bar() {
     let arr = new_array();
     // even numbers
     // closure in filter: FnMut(&I) -> bool
-    // ** consumes arr **
+    // ** into_iter(): consumes arr **
+    // ref (https://www.reddit.com/r/rust/comments/3bmua6/can_someone_help_me_understand_stditerfilter/)
     let arr1: Vec<i32> = arr.into_iter().filter(|&n| n % 2 == 0).collect();
     assert_eq!(Vec::from([2, 4, 6]), arr1);
 
