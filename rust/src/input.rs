1d4fa3b5c2d561d5d2892d06bde735e6b234cf88
rudy3091 <grayblack313@gmail.com>
2022-01-31 19:46:40 +0900

    Update rust/input.rs

--- rust/src/input.rs
+++ rust/src/input.rs
@@ -1,5 +1,37 @@
 use std::io::{self, BufRead};
 
+///
+/// ### example
+/// ```
+/// fn main() {
+///     let s = read_string!();
+///     println!("your input: {}", s);
+///
+///     let a = read_int!();
+///     let b = read_int!();
+///     println!("{} + {} = {}", a, b, a + b);
+/// }
+/// ```
+///
+macro_rules! read_string {
+    () => {{
+        let stdin = io::stdin();
+        let mut buf = String::new();
+        stdin.lock().read_line(&mut buf).unwrap();
+        buf.truncate(buf.len() - 1);
+        buf
+    }};
+}
+
+macro_rules! read_int {
+    () => {{
+        let s = read_string!();
+        let n: i32 = s.parse().unwrap();
+        n
+    }};
+}
+
+
 fn main() {
     let stdin = io::stdin();
     let mut buf = String::new();
